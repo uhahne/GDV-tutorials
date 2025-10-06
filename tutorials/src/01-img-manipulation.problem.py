221246db7a06458bc0ef28153b199aa1901c9795
Uwe Hahne <uhahne@gmail.com>
2025-10-06 17:16:00 +0200

    tut01

--- tutorials/src/01-img-manipulation.problem.py
+++ tutorials/src/01-img-manipulation.problem.py
@@ -3,11 +3,13 @@
 #
 # Load, resize and rotate an image. And display it to the screen.
 
-# TODO First step is to 'import' the opencv module which is called 'cv2'.
+# First step is to 'import' the opencv module which is called 'cv2'.
+import cv2
 
-# TODO Check the opencv version and display it with 'print'.
+# Check the opencv version and display it with 'print'.
+print(cv2.__version__)
 
-# TODO Load an image with image reading modes using 'imread'.
+# Load an image with image reading modes using 'imread'.
 # cv2.IMREAD_UNCHANGED  - If set, return the loaded image as is (with alpha
 #                         channel, otherwise it gets cropped). Ignore EXIF
 #                         orientation.
@@ -15,15 +17,23 @@
 #                         grayscale image (codec internal conversion).
 # cv2.IMREAD_COLOR      - If set, always convert image to the 3 channel BGR
 #                         color image.
+img = cv2.imread("../data/images/logo.png")
 
-# TODO Check if image is loaded fine and raise a FileNotFoundError if not.
+# Check if image is loaded fine and raise a FileNotFoundError if not.
+if img is None:
+    raise FileNotFoundError("image file not found")
 
-# TODO Resize image with 'resize'.
+# Resize image with 'resize'.
+img_larger = cv2.resize(img, (300,400))
 
 # TODO Rotate image (but keep it rectangular) with 'rotate'.
+img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
 
-# TODO Save image with 'imwrite'.
+# Save image with 'imwrite'.
+cv2.imwrite("output.jpg", img)
 
-# TODO Show the image with 'imshow'.
+# Show the image with 'imshow'.
+cv2.imshow("my image", img)
 
-# TODO Avoid the immediate closing of the image window with 'waitKey'.
+# Avoid the immediate closing of the image window with 'waitKey'.
+cv2.waitKey(0)
